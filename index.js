687c49b008b9be0f8c93a063d10f53dcfab8fd51
PepperMcLean <jackmcl206@gmail.com>
2022-01-16 22:51:23 -0600

    commit tree f6272ed44f3bfab02badc30373c8f320c53118e5

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+var multiply = num1 * num2
+
+var random = Math.ceil(Math.random())
+
+let num3 = 60
+let num4 = 8
+var mod = (num3 % num4) 
+
+var max = Math.max(20)
